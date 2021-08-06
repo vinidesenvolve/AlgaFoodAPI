ba35678120ad7aed2d205d4c6696fd7a6f58f8c1
vinidesenvolve <vinidesenvolve@gmail.com>
2021-08-06 11:58:52 -0300

    Arquivo 'import.sql' criado com dados para 'Cozinha'

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into cozinha (nome) values ('Tailandesa');
+insert into cozinha (nome) values ('Irlandesa');
+insert into cozinha (nome) values ('Francesa');
+insert into cozinha (nome) values ('Portuguesa');
