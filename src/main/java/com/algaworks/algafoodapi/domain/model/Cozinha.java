3f891cf4a17bf2b7b4a01374154842dc129d5fee
vinidesenvolve <vinidesenvolve@gmail.com>
2021-08-06 11:53:53 -0300

    'Cozinha' e 'Restaurante' Ids auto generated

--- src/main/java/com/algaworks/algafoodapi/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafoodapi/domain/model/Cozinha.java
@@ -1,6 +1,8 @@
 package com.algaworks.algafoodapi.domain.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.EqualsAndHashCode;
@@ -14,6 +16,7 @@ import lombok.Setter;
 public class Cozinha {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
 
--- src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.EqualsAndHashCode;
@@ -17,6 +19,7 @@ import lombok.Setter;
 public class Restaurante {
  
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
 
