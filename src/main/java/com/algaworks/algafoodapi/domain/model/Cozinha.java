a4f8f669e8682710d00b3a3c72c2c4a33e511254
vinidesenvolve <vinidesenvolve@gmail.com>
2021-08-06 11:36:44 -0300

    Entidade Cozinha' criada

---
+++ src/main/java/com/algaworks/algafoodapi/domain/model/Cozinha.java
@@ -0,0 +1,21 @@
+package com.algaworks.algafoodapi.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cozinha {
+    
+    @Id
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String nome;
+}
