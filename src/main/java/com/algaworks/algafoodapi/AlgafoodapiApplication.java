2b1c25941728553c376db6c8d45a80df1b45c255
vinidesenvolve <vinidesenvolve@gmail.com>
2021-08-06 11:24:43 -0300

    Projeto base e conexao com DB criadas

---
+++ src/main/java/com/algaworks/algafoodapi/AlgafoodapiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodapiApplication.class, args);
+	}
+
+}
